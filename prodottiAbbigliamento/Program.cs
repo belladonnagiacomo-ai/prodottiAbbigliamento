1e5500290c99a863dd68deb56a386042264b6f47
belladonnagiacomo <belladonna.giacomo@franchettisalviani.net>
2026-04-27 08:58:05 +0200

    mod

--- prodottiAbbigliamento/Program.cs
+++ prodottiAbbigliamento/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string file = "prodotti_abbigliamento.csv";
+            string[] f = File.ReadAllLines(file);
+            List<double> lista = new List<double>();
+            int somma = 0;
+            for(int i = 1;  i < f.Length; i++)
+            {
+                string[] riga = f[i].Split(',');
+                lista.Add(Convert.ToInt32(riga[3]));
+               
+               
+            }
+            foreach(double i in  lista)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
